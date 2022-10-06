13c58775afd0ffd62ea0db07b1bf31d5b364b9c5
Elias Cecetka <elias_2000@web.de>
2022-10-06 17:43:24 +0200

    test

--- src/pages/home/components/about/About.tsx
+++ src/pages/home/components/about/About.tsx
@@ -44,7 +44,7 @@ const About = (props: Props) => {
             }}
             id="aboutText"
           >
-            <h1 className="content-header">Elias Cecetkass</h1>
+            <h1 className="content-header">Elias Cecetkatest</h1>
             <h2 className="glitch">
               &#60; Fullstack Software Developer &#47;&#62;
             </h2>
