43cbccdbc02bd4c3dc32a20418bbbd9a3c68f9d2
Elias Cecetka <elias_2000@web.de>
2022-10-06 18:37:56 +0200

    Animated Title

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.scss";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
+import { Helmet } from "react-helmet";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
+  const [barval, setbarval] = useState("|");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (barval === "|") {
+        setbarval("");
+      } else {
+        setbarval("|");
+      }
+    }, 200);
+
+    return () => clearInterval(interval); // This represents the unmount function, in which you need to clear your interval to prevent memory leaks.
+  }, [barval]);
   return (
     <>
+      <Helmet>
+        <title>Elias Cecetka{barval}</title>
+      </Helmet>
       <div className="blurred"></div>
       <About></About>
       <Skills></Skills>
