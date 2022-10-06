e7b94a558ae4de3dd62b370877a10c13f5cef77e
Elias Cecetka <elias_2000@web.de>
2022-10-07 01:09:12 +0200

    Email form with sending logic

--- src/pages/home/components/contact/Contact.tsx
+++ src/pages/home/components/contact/Contact.tsx
@@ -1,25 +1,51 @@
 import React from "react";
+import "./Contact.scss";
+import emailjs from "@emailjs/browser";
 
 type Props = {};
 
 const Contact = (props: Props) => {
-    return (
-        <div className="container" id="Contact">
-            <div className="content">
-                <h1 className="content-header">Contact</h1>
-                <div id="contactcontent">
-                    <h1>Kontaktieren sie Mich!</h1>
-                    <form>
-                        <label>name</label>
-                        <input type="text" name="name" />
-
-                        <label>email</label>
-                        <input type="email" name="user_email" />
-                    </form>
-                </div>
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_4ntabux",
+        "template_fjqf614",
+        e.target as HTMLFormElement,
+        "wuvsr_IMLEX9lfAIM"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="container" id="Contact">
+      <div className="content">
+        <div className="contactcontent">
+          <h1>Habe ich ihr Interesse geweckt?</h1>
+          <h2>Kontaktieren sie Mich!</h2>
+          <form onSubmit={sendEmail}>
+            <div className="contactform">
+              <div className="personinfo">
+                <label>Name</label>
+                <input type="text" name="from_name" />
+                <label>Email</label>
+                <input type="email" name="reply_to" />
+              </div>
+              <div className="message">
+                <label>Nachricht</label>
+                <textarea name="message" />
+              </div>
+              <button type="submit">Senden</button>
             </div>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
